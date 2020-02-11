4b0ad597d3c28c29c7e67fb5a28d87ce1612a1f1
pavmin <pavels.minckovskis@mogofinance.com>
2020-02-11 15:28:54 +0200

    initial commit

--- services/client_gui/index.js
+++ services/client_gui/index.js
@@ -1,14 +1,43 @@
-const http = require('http');
+import http from 'http';
+import amqp from 'amqplib';
+import express from 'express';
 
+const app = express();
 const hostname = '0.0.0.0';
 const port = process.env.PORT;
+const q = 'hello';
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World from client side');
+function bail(err) {
+    console.error(err);
+    process.exit(1);
+}
+
+function queue() {
+    amqp.connect({
+        protocol: 'amqp',
+        hostname: 'queue',
+        port: 5672,
+        username: 'admin',
+        password: 'YourStrongPasswort'
+    }).then(function(conn) {
+        return conn.createChannel().then(function(ch) {
+            let msg = 'Hello World!';
+
+            let ok = ch.assertQueue(q, {durable: false});
+
+            return ok.then(function(_qok) {
+                ch.sendToQueue(q, Buffer.from(msg));
+                console.log(" [x] Sent '%s'", msg);
+                return ch.close();
+            });
+        }).finally(function() { conn.close(); });
+    }).catch(console.warn);
+}
+
+app.get('/', (req, res) => res.send('Hello World from client side!'));
+app.get('/send', (req, res) => {
+    queue();
+    return res.send('Hello World from client side!');
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- services/users/index.php
+++ services/users/index.php
@@ -2,10 +2,27 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 
+$connection = null;
 $connection = new AMQPStreamConnection('queue', 5672, 'admin', 'YourStrongPasswort');
 $channel = $connection->channel();
 
 $channel->queue_declare('hello', false, false, false, false);
 
+$channel->basic_consume('hello', 'tag', false, false, false, false, 'say_hello');
+
+function say_hello(AMQPMessage $message): void
+{
+    echo " [x] Received message. " . $message->getBody() . "\n";
+}
+
 echo " [*] Waiting for messages. To exit press CTRL+C\n";
+
+while (count($channel->callbacks)) {
+    try {
+        $channel->wait();
+    } catch (Exception $e) {
+        echo $e;
+    }
+}
